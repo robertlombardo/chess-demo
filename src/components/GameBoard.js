72e33cc5f9d575adde5ec7f1a15a67f8b343bc2e
Robert Lombardo <rob@wakefield-studios.com>
2017-11-16 20:48:57 -0800

    game piece sprites in initial positions

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -8,6 +8,20 @@ export class GameBoard extends React.Component {
   render() {
   	const BOARD_SIZE = Math.min( this.props.ui.screenWidth, this.props.ui.screenHeight );
   	const SQUARE_SIZE = BOARD_SIZE/8;
+  	const IMG_COORDS_BY_PIECE = {
+		white_king: { x:SQUARE_SIZE*0, y:0 },
+		white_queen: { x:SQUARE_SIZE*1, y:0 },
+		white_bishop: { x:SQUARE_SIZE*2, y:0 },
+		white_knight: { x:SQUARE_SIZE*3, y:0 },
+		white_rook: { x:SQUARE_SIZE*4, y:0 },
+		white_pawn: { x:SQUARE_SIZE*5, y:0 },
+		black_king: { x:SQUARE_SIZE*0, y:SQUARE_SIZE },
+		black_queen: { x:SQUARE_SIZE*1, y:SQUARE_SIZE },
+		black_bishop: { x:SQUARE_SIZE*2, y:SQUARE_SIZE },
+		black_knight: { x:SQUARE_SIZE*3, y:SQUARE_SIZE },
+		black_rook: { x:SQUARE_SIZE*4, y:SQUARE_SIZE },
+		black_pawn: { x:SQUARE_SIZE*5, y:SQUARE_SIZE }
+	}
 
     return (
 		<Table className="game-board" style={{width:BOARD_SIZE}}>
@@ -17,7 +31,9 @@ export class GameBoard extends React.Component {
 						<tr style={{height:SQUARE_SIZE}} key={i}>
 							{row.map((square,i)=>{
 								return(
-									<td className={square.color+"-square"} style={{width:SQUARE_SIZE}} key={i}></td>
+									<td className={"square "+square.color+"-square"} style={{width:SQUARE_SIZE,height:SQUARE_SIZE}} key={i}>
+										{getGamePiece(square)}
+									</td>								
 								)
 							})}
 						</tr>
@@ -26,6 +42,22 @@ export class GameBoard extends React.Component {
 		    </tbody>
 		</Table>
     );
+
+    function getGamePiece( square ) {
+    	if( square.piece ) {
+    		return(
+    			<img className="game-piece" 
+    				 src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Chess_Pieces_Sprite.svg/2000px-Chess_Pieces_Sprite.svg.png" 
+    				 style={{
+    				 	width: SQUARE_SIZE*6,
+    				 	height: SQUARE_SIZE*2,
+    				 	left: -IMG_COORDS_BY_PIECE[ square.piece.color+"_"+square.piece.type ].x,
+    				 	top: -IMG_COORDS_BY_PIECE[ square.piece.color+"_"+square.piece.type ].y
+    				 }}
+    			/>
+    		)
+    	}
+    }
   }
 }
 
--- src/reducers/gameBoardReducer.js
+++ src/reducers/gameBoardReducer.js
@@ -7,10 +7,44 @@ for( var x = 0; x < 8; ++x ) {
       initialBoardState[x][y] = {
         color: (x%2)===(y%2)? "white" : "black",
         x,
-        y
+        y,
+        piece: null
       }
     }
   }
+
+const piecesByFile = [
+  "rook",
+  "knight",
+  "bishop",
+  "queen",
+  "king",
+  "bishop",
+  "knight",
+  "rook"
+]
+
+for( var i = 0; i < 8; ++i ) {  
+  initialBoardState[1][i].piece = {
+    type: "pawn",
+    color: "black"
+  }
+
+  initialBoardState[6][i].piece = {
+    type: "pawn",
+    color: "white"
+  }
+
+  initialBoardState[0][i].piece = {
+    type: piecesByFile[i],
+    color: "black"
+  }
+
+  initialBoardState[7][i].piece = {
+    type: piecesByFile[i],
+    color: "white"
+  }
+}
 
 export default function game_board(state = initialBoardState, action) {
 
